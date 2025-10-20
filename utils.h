eaec683a8912e7e7f2208e7f8617cb7790c44615
OneMoveBRB <sh_amir2014@mail.ru>
2025-10-20 19:15:00 +0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -8,7 +8,9 @@ void Skip_Spaces(Assembler* ASM, size_t* i, size_t* asm_line = NULL);
 int SkipWord(Assembler* ASM, size_t* i);
 
 char* GetWord(Assembler* ASM, size_t* i);
+
 AssemblerErr_t StackPushCommand(Assembler* ASM, size_t* i, Instruction* Commands, size_t Commands_size, size_t* command_type);
+AssemblerErr_t StackPushLabel(Assembler* ASM, size_t* i, Stack_t* Labels);
 // AssemblerErr_t DefineCommand(Assembler* ASM, size_t* i, Instruction* Commands, size_t Commands_size, size_t* command_index);
 
 size_t BinSearch(void* arr, size_t num, size_t element_size, void* value, int (*cmp)(const void*, const void*));
