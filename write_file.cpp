27ec77fb5fc2db63096491ff7392423a6d8a52f5
OneMoveBRB <sh_amir2014@mail.ru>
2025-10-20 21:07:23 +0300

    Create write_file.cpp

---
+++ write_file.cpp
@@ -0,0 +1,25 @@
+#include "write_file.h"
+
+#include <stdio.h>
+
+size_t WriteFile(const int* buffer, size_t buffer_size, const char* file_name) {
+    FILE* fp = fopen(file_name, "wb");
+    if (fp == NULL) {
+        fprintf(stderr, "Opening %s [%s]: fp_from = NULL\n", file_name, __func__);
+        return 0;
+    }
+
+    if (fwrite(&buffer_size, sizeof(size_t), 1, fp) != 1) {
+        fprintf(stderr, "Writing OutputBuffer_size FAILED\n");
+        return 0;
+    }
+
+    printf("Wrote OutputBuffer_size\n");
+
+    size_t num_of_wrote_elements = fwrite(buffer, sizeof(int), buffer_size, fp);
+    printf("Wrote OutputBuffer: %zu elements out of %zu\n", num_of_wrote_elements,  buffer_size);
+
+    fclose(fp);
+
+    return num_of_wrote_elements;
+}
