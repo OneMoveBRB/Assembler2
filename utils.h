6495a8a83fe4648a93a211f1ea466a169cf7a360
OneMoveBRB <sh_amir2014@mail.ru>
2025-10-20 18:45:06 +0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -12,7 +12,9 @@ AssemblerErr_t StackPushCommand(Assembler* ASM, size_t* i, Instruction* Commands
 // AssemblerErr_t DefineCommand(Assembler* ASM, size_t* i, Instruction* Commands, size_t Commands_size, size_t* command_index);
 
 size_t BinSearch(void* arr, size_t num, size_t element_size, void* value, int (*cmp)(const void*, const void*));
+
 void* move_ptr(void* arr, size_t offset, size_t element_size);
 int strcmp_(const void* a, const void* b);
+int CommandsCompare(const void* a, const void* b);
 
 #endif
