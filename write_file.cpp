e06b2ee3e53252ab71bcde1687081eba115654d2
OneMoveBRB <sh_amir2014@mail.ru>
2025-10-25 12:51:44 +0300

    Update write_file.cpp

--- write_file.cpp
+++ write_file.cpp
@@ -2,12 +2,19 @@
 
 #include <stdio.h>
 
-size_t WriteFile(const int* buffer, size_t buffer_size, const char* file_name) {
+size_t WriteFile(const int* buffer, size_t buffer_size, size_t StartIP, const char* file_name) {
     FILE* fp = fopen(file_name, "wb");
     if (fp == NULL) {
         fprintf(stderr, "Opening %s [%s]: fp_from = NULL\n", file_name, __func__);
         return 0;
     }
+
+    if (fwrite(&StartIP, sizeof(size_t), 1, fp) != 1) {
+        fprintf(stderr, "Writing StartIP FAILED\n");
+        return 0;
+    }
+
+    printf("Wrote StartIP\n");
 
     if (fwrite(&buffer_size, sizeof(size_t), 1, fp) != 1) {
         fprintf(stderr, "Writing OutputBuffer_size FAILED\n");
